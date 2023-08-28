b195e2e8495cf40faafabef1d59985619c3b4539
Hiroki Kato <hirokikato1@gmail.com>
2023-08-28 17:52:49 -0400

    completes lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,47 @@
+import ipdb
+
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_artist(artist)
+        self.add_to_genre(genre)
+        self.add_to_artist_count(artist)
+        self.add_to_genre_count(genre)
+        
+    
+    @classmethod
+    def add_song_to_count(cls, increment=1):
+        cls.count += increment
+
+    @classmethod
+    def add_to_artist(cls, artist):
+        if not artist in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre(cls, genre):
+        if not genre in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if cls.artist_count.get(artist, False):
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if cls.genre_count.get(genre, False):
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
